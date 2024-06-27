81c57c02566443364eaef792e698f871376c305b
techwura <afiaserwaaamponsem@gmail.com>
2024-06-27 15:33:07 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""shebang line"""
+
+class Square:
+    """A class that represent a square"""
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
